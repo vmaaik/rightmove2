c1a5794810f6305b11acb52d33ce3b77b7cba199
raps <vmaaik@gmail.com>
2023-10-14 14:04:28 +0100

    html output

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+import sched
+import threading
+import time
+
+import requests
 from flask import Flask, render_template
 from geojson_scraper import RightmoveData
 import json
@@ -6,25 +11,22 @@ import logging
 app = Flask(__name__)
 
 # Configure logging
-logging.basicConfig(filename='app.log', level=logging.DEBUG)
+logging.basicConfig(filename='app2.log', level=logging.DEBUG)
 
-# Example URLs
 urls = [
-    "https://www.rightmove.co.uk/property-for-sale/find.html?minBedrooms=2&propertyTypes=detached%2Csemi-detached%2Cterraced%2Cbungalow&keywords=&sortType=2&viewType=LIST&channel=BUY&index=0&maxPrice=350000&radius=0.0&maxDaysSinceAdded=1&locationIdentifier=USERDEFINEDAREA%5E%7B%22polylines%22%3A%22q%7BrdIzkhNgpH%7CoEwrDojGmkFq%7EVb%60BeqPlaAapJp%7DCslCnxCq%7CH%60Ve%7EEqxE%7BiKleJsrSjcN%7DqBlSr_e%40%7CsCruE%7CqHylDbq%40xiK%7EEpwPgsBztXkgFxnZijQixH%22%7D",
-    "https://www.rightmove.co.uk/property-for-sale/find.html?minBedrooms=2&propertyTypes=detached%2Csemi-detached%2Cterraced%2Cbungalow&keywords=&sortType=2&viewType=LIST&channel=BUY&index=0&maxPrice=350000&radius=0.0&maxDaysSinceAdded=1&locationIdentifier=USERDEFINEDAREA%5E%7B%22polylines%22%3A%22kmreIpgrKspAqaAuzBvXajBhRcsAdPmgBa%5D%7BhAajAcn%40qnBhNc%60Ejo%40adClgBuaBhkBc%7B%40%60xBse%40bjBnT%7EhAvv%40rbAllBr%5DjhC%7CUv_CeIjsE%7BPbyAyp%40uI%22%7D"
+    "https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=USERDEFINEDAREA%5E%7B%22polylines%22%3A%22q%7BrdIzkhNgpH%7CoEwrDojGmkFq~Vb%60BeqPlaAapJp%7DCslCnxCq%7CH%60Ve~EqxE%7BiKleJsrSjcN%7DqBlSr_e%40%7CsCruE%7CqHylDbq%40xiK~EpwPgsBztXkgFxnZijQixH%22%7D&minBedrooms=2&maxPrice=400000&propertyTypes=detached%2Csemi-detached%2Cterraced%2Cbungalow&maxDaysSinceAdded=14&mustHave=&dontShow=&furnishTypes=&keywords=",
+    "https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=USERDEFINEDAREA%5E%7B%22polylines%22%3A%22kmreIpgrKspAqaAuzBvXajBhRcsAdPmgBa%5D%7BhAajAcn%40qnBhNc%60Ejo%40adClgBuaBhkBc%7B%40%60xBse%40bjBnT~hAvv%40rbAllBr%5DjhC%7CUv_CeIjsE%7BPbyAyp%40uI%22%7D&minBedrooms=2&maxPrice=400000&propertyTypes=detached%2Csemi-detached%2Cterraced%2Cbungalow&maxDaysSinceAdded=14&mustHave=&dontShow=&furnishTypes=&keywords="
 ]
 
+
 # Cache variable to store the GeoJSON data
-cache = None
 
 def scrape_data():
-    # Scrape data from each URL
     results = []
     for url in urls:
         data = RightmoveData(url)
         results.extend(data.get_results["features"])
 
-    # Merge GeoJSON features into one GeoJSON object
     geojson_obj = {
         "type": "FeatureCollection",
         "features": results
@@ -33,25 +35,69 @@ def scrape_data():
     # Convert to JSON string
     geojson_str = json.dumps(geojson_obj)
 
-    logging.debug(f"Scraped GeoJSON: {geojson_str}")
-
     return geojson_str
+
 
 def populate_cache():
     global cache
-    if cache is None:
-        # Get the GeoJSON data if it's not cached
-        cache = scrape_data()
+    cache = None
+    cache = scrape_data()
 
 @app.route('/')
 def index():
     logging.debug("Rendering index page")
-    return render_template('index.html', geojson=cache)
+    
+    # Generate the HTML content by rendering the template
+    html_content = render_template('index.html', geojson=cache)
+    
+    # Save the HTML content to a file (e.g., output.html)
+    with open('output.html', 'w') as file:
+        file.write(html_content)
+
+    return html_content
+
+
+@app.route('/populate')
+def populate():
+    logging.debug("Populating cache")
+    populate_cache()
+    logging.debug("Cache has been populated")
+
+
+def scheduled_job():
+    try:
+        # Make an HTTP GET request to your own endpoint to populate the cache
+        response = requests.get('http://127.0.0.1:5000/')
+        if response.status_code == 200:
+            logging.debug("Healthcheck done")
+    except Exception as e:
+        logging.error(f"Error calling scheduled job: {str(e)}")
+
+
+def scheduled_cache_populate():
+    while True:
+        # Populate the cache every 6 hours (6 * 60 * 60 seconds)
+        time.sleep(6 * 60 * 60)
+        logging.debug("Scheduled cache population started")
+        populate_cache()
+        logging.debug("Scheduled cache population done")
 
 
 if __name__ == '__main__':
     # Populate the cache before starting the Flask server
     populate_cache()
 
-    # Run the Flask server
-    app.run(host='0.0.0.0', port=5000)
+    # Start the Flask server in a separate thread
+    server_thread = threading.Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 5000})
+    server_thread.start()
+
+    # Start the scheduled job to populate the cache every 6 hours
+    cache_populate_thread = threading.Thread(target=scheduled_cache_populate)
+    cache_populate_thread.start()
+
+    # Start the scheduled job for health check
+    scheduler = sched.scheduler(time.time, time.sleep)
+    while True:
+        scheduler.enter(60, 1, scheduled_job, ())
+        scheduler.run()
+
--- geojson_scraper.py
+++ geojson_scraper.py
@@ -1,15 +1,7 @@
-import datetime
-import numpy as np
-import pandas as pd
 import requests
-from lxml import html
+from bs4 import BeautifulSoup
 import json
 from geopy.geocoders import Nominatim
-import smtplib
-from email.mime.text import MIMEText
-import logging
-from geopy.distance import geodesic
-
 
 class RightmoveData:
     def __init__(self, url: str, get_floorplans: bool = False):
@@ -20,13 +12,10 @@ class RightmoveData:
 
     @staticmethod
     def _request(url: str):
-        print(f"Making request to URL: {url}")
         r = requests.get(url)
-        return r.status_code, r.content
+        return r.status_code, r.text
 
     def _validate_url(self):
-        """Basic validation that the URL at least starts in the right format and
-        returns status code 200."""
         print(f"Validating URL: {self.url}")
         real_url = "{}://www.rightmove.co.uk/{}/find.html?"
         protocols = ["http", "https"]
@@ -47,53 +36,36 @@ class RightmoveData:
 
     @property
     def results_count_display(self):
-        """Returns an integer of the total number of listings as displayed on
-        the first page of results. Note that not all listings are available to
-        scrape because rightmove limits the number of accessible pages."""
-        tree = html.fromstring(self._first_page)
-        xpath = """//span[@class="searchHeader-resultCount"]/text()"""
-        return int(tree.xpath(xpath)[0].replace(",", ""))
+        soup = BeautifulSoup(self._first_page, 'html.parser')
+        result_count = soup.find('span', class_='searchHeader-resultCount')
+        return int(result_count.text.replace(",", ""))
 
     @property
     def page_count(self):
-        """Returns the number of result pages returned by the search URL. There
-        are 24 results per page. Note that the website limits results to a
-        maximum of 42 accessible pages."""
         page_count = self.results_count_display // 24
         if self.results_count_display % 24 > 0:
             page_count += 1
-        # Rightmove will return a maximum of 42 results pages, hence:
         if page_count > 42:
             page_count = 42
         return page_count
 
     def _get_page(self, request_content: str, get_floorplans: bool = False):
-        """Method to scrape data from a single page of search results. Used
-        iteratively by the `get_results` method to scrape data from every page
-        returned by the search."""
-        # Process the html:
-        tree = html.fromstring(request_content)
-        script_tag = tree.xpath('//script[contains(text(), "window.jsonModel")]/text()')
-
+        soup = BeautifulSoup(request_content, 'html.parser')
+        script_tag = soup.find('script', text=lambda x: 'window.jsonModel' in str(x))
         if script_tag:
-            json_model = script_tag[0].split('window.jsonModel = ')[-1].rstrip(';')
+            json_model = script_tag.text.split('window.jsonModel = ')[-1].rstrip(';')
 
         data = json.loads(json_model)
         properties = data['properties']
-
         features = []
 
         geolocator = Nominatim(user_agent="my-app")
-        # Extract the required information from each property
         for property_data in properties:
             display_addresses = property_data['displayAddress']
             property_sub_type = property_data['propertySubType']
             prices = property_data['price']['displayPrices'][0]['displayPrice']
             latitude = property_data['location']['latitude']
             longitude = property_data['location']['longitude']
-            location = geolocator.reverse((latitude, longitude), exactly_one=True)
-            address = location.raw['address']
-            town = address.get('village') or address.get('town') or address.get('city') or ''
             agency = property_data['formattedBranchName']
             first_visible = property_data['firstVisibleDate']
             property_id = property_data['id']
@@ -111,7 +83,6 @@ class RightmoveData:
                 },
                 "properties": {
                     'id': property_id,
-                    'area': town,
                     'type': property_sub_type,
                     'beds': bedrooms,
                     'price': prices,
@@ -130,22 +101,14 @@ class RightmoveData:
         return features
 
     def _get_results(self, get_floorplans: bool = False):
-        """Build a GeoJSON FeatureCollection with all results returned by the search."""
         results = self._get_page(self._first_page, get_floorplans=get_floorplans)
 
-        # Iterate through all pages scraping results:
         for p in range(1, self.page_count + 1, 1):
-            print(f"Scraping page {p} of {self.page_count}")
-            # Create the URL of the specific results page:
             p_url = f"{str(self.url)}&index={p * 24}"
-
-            # Make the request:
             status_code, content = self._request(p_url)
 
-            # Requests to scrape lots of pages eventually get status 400, so:
             if status_code != 200:
                 break
-            # Create a temporary FeatureCollection of page results:
             temp_results = self._get_page(content, get_floorplans=get_floorplans)
             results.extend(temp_results)
 
@@ -156,5 +119,5 @@ class RightmoveData:
 
     @staticmethod
     def _clean_results(results: dict):
-        # No cleaning needed in this case, so return the results as is
         return results
+
